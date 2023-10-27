a157958e8bf4d9021f02ed05247ce5c6014d475a
monchrome <monchrome@users.noreply.github.com>
2023-10-27 11:10:59 -0700

    Added comment  AWSStsTokensProvider.java

--- src/api/AWSStsTokensProvider.java
+++ src/api/AWSStsTokensProvider.java
@@ -7,6 +7,8 @@ import exceptions.StsOperationException;
 
 import java.util.List;
 
+/* AWS STS Token Provider interface */
+
 public interface AWSStsTokensProvider {
      AWSSTSToken getSTSToken(List<PolicyParameters> policyParametersList, int timeToLiveInMinutes, String region)
              throws StsOperationException, PolicyGenerationException;
